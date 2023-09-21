308c018d67b4b8da2c66eea82a7f0bb4099db55c
luanavefago1@gmail.com <luana.santos@dynamox.net>
2023-09-21 08:09:56 -0300

    feat: add transactions table and queries with knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import fastify from 'fastify'
 import { knex } from './database'
+// import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // Inserção
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
 
-  return tables
+  // Busca
+
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app.listen({ port: 3333 }).then(() => {
