f31c779effb5fddb841ceacc3f4e4c562c78d079
luanavfg <luanavefago1@gmail.com>
2023-09-27 08:02:15 -0300

    feat: add fastify plugins

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,22 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 // import crypto from 'node:crypto'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // Inserção
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000,
-  //   })
-  //   .returning('*')
-
-  // Busca
-
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transactions
-})
+app.register(transactionsRoutes)
+// app.get('/hello', async () => {
+//   Inserção
+//   const transaction = await knex('transactions')
+//     .insert({
+//       id: crypto.randomUUID(),
+//       title: 'Transação de teste',
+//       amount: 1000,
+//     })
+//     .returning('*')
+//   return transaction
+// })
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running')
